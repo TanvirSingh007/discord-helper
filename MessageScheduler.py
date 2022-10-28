04c00b65e135c4dbae5ed3fe1c15a4d82053e31d
Tanvir <technogeek13559@gmail.com>
2022-10-29 00:25:50 +0530

    Fixed bot reading its own messages

--- MessageScheduler.py
+++ MessageScheduler.py
@@ -1,30 +1,30 @@
 import asyncio
-from http import server
-from time import sleep
-from click import command
 import discord
 import datetime
 from decouple import config
 import json
-import os
 
 TOKEN = config("BOT_TOKEN")
+ID = int(config("BOT_ID"))
 client = discord.Client(intents=discord.Intents.all())
 
 commandlist = ["-help", '-list', '-schedule', '-delete', '-info', '-time']
 
-def isInteger (string):
+
+def isInteger(string):
     try:
         int(string)
         return True
     except:
         return False
 
+
 def createMessagesFile():
     f = open('messages.json', 'w')
-    data = { }
-    json.dump(data, f, indent = 2)
+    data = {}
+    json.dump(data, f, indent=2)
     f.close()
+
 
 def loadMessages():
     try:
@@ -37,11 +37,13 @@ def loadMessages():
     f.close()
     return data
 
+
 def saveMessages(data):
     f = open('messages.json', 'w')
-    json.dump(data, f, indent = 2)
+    json.dump(data, f, indent=2)
     f.close()
     getScheduledTime()
+
 
 def listMessage(message):
     data = loadMessages()
@@ -54,13 +56,16 @@ def listMessage(message):
             retval = 'You have the following scheduled messages: \n'
             index = 1
             for scheduledMessage in data[serverid][userid]:
-                if(scheduledMessage["Active"]):
-                    retval = retval + str(index) + ':```' + scheduledMessage["Message"] + '``` is scheduled at ' + (datetime.datetime.strptime(scheduledMessage["Schedule Time"], '%d/%m/%Y %H:%M')).strftime('%d-%B-%Y %H:%M') + ' in <#' + scheduledMessage["Channel"] + '>\n'
+                if (scheduledMessage["Active"]):
+                    retval = retval + str(index) + ':```' + scheduledMessage["Message"] + '``` is scheduled at ' + (
+                        datetime.datetime.strptime(scheduledMessage["Schedule Time"], '%d/%m/%Y %H:%M')).strftime(
+                        '%d-%B-%Y %H:%M') + ' in <#' + scheduledMessage["Channel"] + '>\n'
                 index += 1
             return retval
 
     except:
         return 'You have no scheduled messages'
+
 
 def delMessage(message):
     data = loadMessages()
@@ -68,12 +73,12 @@ def delMessage(message):
     userid = str(message.author.id)
     try:
         lastIndex = len(data[serverid][userid])
-        if(len(message.content.split(' ')) != 2):
+        if (len(message.content.split(' ')) != 2):
             return 'Incorrect format used. Use ```-help``` to learn more '
         else:
-            if(isInteger(message.content.split(' ')[1])):
+            if (isInteger(message.content.split(' ')[1])):
                 try:
-                    if(int(message.content.split(' ')[1]) < 1):
+                    if (int(message.content.split(' ')[1]) < 1):
                         raise Exception
                     data[serverid][userid].pop(int(message.content.split(' ')[1]) - 1)
                     saveMessages(data)
@@ -85,46 +90,47 @@ def delMessage(message):
     except:
         return 'You have no scheduled messages'
 
+
 def scheduleMessage(message):
     serverid = str(message.guild.id)
     userid = str(message.author.id)
     try:
-        if(len(message.content.split("'''"))!=3):
+        if (len(message.content.split("'''")) != 3):
             raise Exception
-        
+
         schMessage = message.content.split("'''")[1]
-        if(message.content.split("'''")[0] != '-schedule '):
+        if (message.content.split("'''")[0] != '-schedule '):
             raise Exception
-        
+
         args = message.content.split("'''")[2].split(' ')
-        if(args[0] == ''):
+        if (args[0] == ''):
             args.pop(0)
         numArgs = len(args)
-        if(not (numArgs == 2 or numArgs == 3 or numArgs == 4)):
+        if (not (numArgs == 2 or numArgs == 3 or numArgs == 4)):
             raise Exception
-        
+
         time = datetime.datetime.strptime(args[0] + ' ' + args[1], '%d/%m/%Y %H:%M')
         channel = str(message.channel.id)
         delayInMins = 0
-        
-        if(numArgs > 2):
-            if(args[2][0:2] == '<#' and args[2][-1] == '>'):
+
+        if (numArgs > 2):
+            if (args[2][0:2] == '<#' and args[2][-1] == '>'):
                 channel = args[2][2:-1]
-                if(numArgs == 4):
+                if (numArgs == 4):
                     delayInMins = int(args[3])
             else:
                 channel = str(message.channel.id)
                 delayInMins = int(args[2])
-        
+
         while '```' in schMessage:
             i = schMessage.find('```')
-            schMessage = schMessage[0:i] + schMessage[i+3:]
-        
+            schMessage = schMessage[0:i] + schMessage[i + 3:]
+
         while schMessage[0] == '`':
             schMessage = schMessage[1:]
-        
-        while schMessage[len(schMessage)-1] == '`':
-            schMessage = schMessage[0:len(schMessage)-2]
+
+        while schMessage[len(schMessage) - 1] == '`':
+            schMessage = schMessage[0:len(schMessage) - 2]
 
         newschedule = {
             "Message": schMessage,
@@ -137,8 +143,8 @@ def scheduleMessage(message):
 
         messageData = loadMessages()
 
-        if(serverid in messageData.keys()):
-            if(userid in messageData[serverid].keys()):
+        if (serverid in messageData.keys()):
+            if (userid in messageData[serverid].keys()):
                 messageData[serverid][userid].append(newschedule)
             else:
                 messageData[serverid][userid] = [newschedule]
@@ -148,20 +154,22 @@ def scheduleMessage(message):
             }
             messageData[serverid] = newuser
 
-        if(len(messageData[serverid][userid]) < 10):
+        if (len(messageData[serverid][userid]) < 10):
             saveMessages(messageData)
             return 'Message Scheduled'
-        
+
         else:
             return 'Message Limit Reached'
 
     except:
-        return 'Incorrect format, use -help to know more'    
+        return 'Incorrect format, use -help to know more'
 
-async def sendmessage (channel, message):
+
+async def sendmessage(channel, message):
     channel = client.get_channel(int(channel))
     await channel.send(message)
-    
+
+
 async def parseCommand(message):
     command = message.content
     info = '''
@@ -181,18 +189,19 @@ async def parseCommand(message):
               Usage: -delete [index]
   -time     : Displays server time```'''
 
-    if(command.split(' ')[0] == '-help'):
-        await sendmessage (message.channel.id, help)
-    elif(command.split(' ')[0] == '-delete'):
-        await sendmessage (message.channel.id, delMessage(message))
-    elif(command.split(' ')[0] == '-list'):
-        await sendmessage (message.channel.id, listMessage(message))
-    elif(command.split(' ')[0] == '-schedule'):
-        await sendmessage (message.channel.id, scheduleMessage(message))
-    elif(command.split(' ')[0] == '-info'):
-        await sendmessage (message.channel.id, info)
-    elif(command.split(' ')[0] == '-time'):
-        await sendmessage (message.channel.id, datetime.datetime.now().strftime('%d-%B-%Y %H:%M'))
+    if (command.split(' ')[0] == '-help'):
+        await sendmessage(message.channel.id, help)
+    elif (command.split(' ')[0] == '-delete'):
+        await sendmessage(message.channel.id, delMessage(message))
+    elif (command.split(' ')[0] == '-list'):
+        await sendmessage(message.channel.id, listMessage(message))
+    elif (command.split(' ')[0] == '-schedule'):
+        await sendmessage(message.channel.id, scheduleMessage(message))
+    elif (command.split(' ')[0] == '-info'):
+        await sendmessage(message.channel.id, info)
+    elif (command.split(' ')[0] == '-time'):
+        await sendmessage(message.channel.id, datetime.datetime.now().strftime('%d-%B-%Y %H:%M'))
+
 
 def getScheduledTime():
     data = loadMessages()
@@ -200,13 +209,14 @@ def getScheduledTime():
     timeDict = {}
     for serverid in data.keys():
         for userid in data[serverid].keys():
-            index  = 0
+            index = 0
             for message in data[serverid][userid]:
                 try:
                     timeDict[(message["Schedule Time"])].append([serverid, userid, index])
                 except:
                     timeDict[(message["Schedule Time"])] = [[serverid, userid, index]]
                 index = index + 1
+
 
 async def sendScheduledMessage(timeInfo):
     global commandQueue
@@ -224,6 +234,7 @@ async def sendScheduledMessage(timeInfo):
                 data[serverid][userid].pop(index)
     saveMessages(data)
 
+
 async def idle():
     global timeDict
     getScheduledTime()
@@ -240,19 +251,23 @@ async def idle():
             except:
                 await asyncio.sleep(1)
 
+
 @client.event
 async def on_ready():
     await idle()
 
+
 @client.event
 async def on_message(message):
     global commandQueue
-    print(message.content)
-    if((str(message.content).split(' '))[0] in commandlist):
-        try:
-            commandQueue.append(message)
-        except:
-            commandQueue = [message]
+    if (message.author.id != ID):
+        print(message.content)
+        if ((str(message.content).split(' '))[0] in commandlist):
+            try:
+                commandQueue.append(message)
+            except:
+                commandQueue = [message]
+
 
 client.run(TOKEN)
 
